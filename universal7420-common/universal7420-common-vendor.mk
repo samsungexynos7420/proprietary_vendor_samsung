7c6b4ee502cc7bbc44d18031b0365545cdedd140
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-02-18 10:05:56 +0000

    add sensorhub patch for note 5

--- universal7420-common/universal7420-common-vendor.mk
+++ universal7420-common/universal7420-common-vendor.mk
@@ -6,6 +6,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/samsung/universal7420-common
 
 PRODUCT_COPY_FILES += \
+    vendor/samsung/universal7420-common/proprietary/bin/bcm_sensorhub_a2.patch:$(TARGET_COPY_OUT_SYSTEM)/bin/bcm_sensorhub_a2.patch \
     vendor/samsung/universal7420-common/proprietary/vendor/bin/cbd:$(TARGET_COPY_OUT_VENDOR)/bin/cbd \
     vendor/samsung/universal7420-common/proprietary/vendor/bin/gps.cer:$(TARGET_COPY_OUT_VENDOR)/bin/gps.cer \
     vendor/samsung/universal7420-common/proprietary/vendor/bin/hw/gpsd:$(TARGET_COPY_OUT_VENDOR)/bin/hw/gpsd \
